f1df9077f0ad929cb002923b3000d95b8f0996c4
Adrian Khaskin <haskin.dev@gmail.com>
2022-09-06 20:31:10 -0400

    initial commit

---
+++ src/main/java/dev/haskin/cookrecipes/CookrecipesApplication.java
@@ -0,0 +1,13 @@
+package dev.haskin.cookrecipes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CookrecipesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CookrecipesApplication.class, args);
+	}
+
+}
