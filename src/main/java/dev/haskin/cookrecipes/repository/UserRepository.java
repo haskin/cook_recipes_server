8a77166d77a234ce1358164c5ca850c65f3bedf9
Adrian Khaskin <haskin.dev@gmail.com>
2022-09-08 13:21:20 -0400

    Added findByUsername()

--- src/main/java/dev/haskin/cookrecipes/repository/UserRepository.java
+++ src/main/java/dev/haskin/cookrecipes/repository/UserRepository.java
@@ -1,9 +1,11 @@
 package dev.haskin.cookrecipes.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import dev.haskin.cookrecipes.model.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-
+    Optional<User> findByUsername(String username);
 }
