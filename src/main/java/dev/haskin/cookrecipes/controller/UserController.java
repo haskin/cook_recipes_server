7325efa792f57be5bee5d59fa1125a934a201c49
Adrian Khaskin <haskin.dev@gmail.com>
2022-09-09 13:05:53 -0400

    added find recipes by user

--- src/main/java/dev/haskin/cookrecipes/controller/UserController.java
+++ src/main/java/dev/haskin/cookrecipes/controller/UserController.java
@@ -1,13 +1,16 @@
 package dev.haskin.cookrecipes.controller;
 
 import java.util.Set;
+import java.util.stream.Collectors;
 
-import org.springframework.security.core.Authentication;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import dev.haskin.cookrecipes.dto.RecipeResponse;
 import dev.haskin.cookrecipes.model.User;
 import dev.haskin.cookrecipes.security.UserPrincipal;
 import dev.haskin.cookrecipes.service.UserService;
@@ -15,6 +18,8 @@ import dev.haskin.cookrecipes.service.UserService;
 @RestController
 @RequestMapping(value = "/api")
 public class UserController {
+    @Autowired
+    private ModelMapper modelMapper;
 
     @Autowired
     private UserService userService;
@@ -23,6 +28,14 @@ public class UserController {
     public String findUser(Authentication authentication) {
         UserPrincipal userPrincipal = (UserPrincipal) authentication.getPrincipal();
         return "hello world";
+    }
+
+    @GetMapping("/user/recipes")
+    public Set<RecipeResponse> findUserRecipes(Authentication authentication) {
+        UserPrincipal userPrincipal = (UserPrincipal) authentication.getPrincipal();
+        User user = userService.findUserById(userPrincipal.getId());
+        return user.getRecipesOwned().stream().map(recipe -> modelMapper.map(recipe, RecipeResponse.class))
+                .collect(Collectors.toSet());
     }
 
     @GetMapping("/users")
