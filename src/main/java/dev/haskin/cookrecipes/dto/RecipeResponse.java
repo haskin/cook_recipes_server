e0719d79b8f0e0331f798a2323bbe364f46e1a33
Adrian Khaskin <haskin.dev@gmail.com>
2022-09-11 19:56:30 -0400

    Added Set of ingredient dto to RecipeResponse dto

--- src/main/java/dev/haskin/cookrecipes/dto/RecipeResponse.java
+++ src/main/java/dev/haskin/cookrecipes/dto/RecipeResponse.java
@@ -1,7 +1,13 @@
 package dev.haskin.cookrecipes.dto;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -22,4 +28,7 @@ public class RecipeResponse {
 
     @NotBlank
     private String image;
+
+    @JsonInclude(Include.NON_NULL)
+    private Set<IngredientRequest> ingredients = new HashSet<>();
 }
